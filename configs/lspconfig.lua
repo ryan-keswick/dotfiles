1293055875f7cf89e689eb1909ac389504e6dc47
Ryan Keswick <ryank415@gmail.com>
2024-03-08 09:08:07 +1100

    Inital Setup For nextJS

---
+++ configs/lspconfig.lua
@@ -0,0 +1,20 @@
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attatch
+local capabilities = base.capabilities
+
+local lspconfig = require("lspconfig")
+
+local servers = {
+  "cssls",
+  "eslint",
+  "tailwindcss", 
+  "tsserver", 
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+     on_attach = on_attach,
+     capabilities = capabilities,
+  }
+end
+
---
+++ plugins.lua
@@ -0,0 +1,75 @@
+local plugins = {
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "prettierd",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+      }
+    }
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {
+      "javascript",
+      "javascriptcreact",
+      "typescript",
+      "typescriptreact",
+    },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      opts = require "plugins.configs.treesitter"
+      opts.ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "css",
+        "dockerfile",
+        "go",
+        "gomod",
+        "gowork",
+        "hcl",
+        "html",
+        "http",
+        "javascript",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "python",
+        "regex",
+        "ruby",
+        "rust",
+        "terraform",
+        "tsx",
+        "toml",
+        "vim",
+        "yaml",
+        "zig",
+      }
+      return opts
+    end
+  },
+}
+return plugins
