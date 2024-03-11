2ff3af1d02fa9ee426821a5d4384ddec652c08eb
Ryan Keswick <ryank415@gmail.com>
2024-03-11 11:33:48 +1100

    Adding line diagnostics

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -1,3 +1,13 @@
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+  callback = function ()
+    vim.diagnostic.open_float(nil, {focus=false})
+  end
+})
+
 local plugins = {
   {
     "christoomey/vim-tmux-navigator",
